5e01d4aafc92881d8281d671ab6307220cffbb82
Pranav Gonuguntla <spgonugu@uci.edu>
2025-03-24 11:53:37 -0700

    initial conv layers

--- gesture_tracking.py
+++ gesture_tracking.py
@@ -1,1 +1,13 @@
-test
+import torch
+import torch.nn as nn
+
+class GestureCNN(nn.Module):  
+    def __init__(self, num_classes=6): # Num_classes depends on the number of different gestures in the dataset (6 is placeholder)
+        super(GestureCNN, self).__init__()  
+        
+        # Convolutional layers
+        self.conv_one = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv_two = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv_three = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+
+
