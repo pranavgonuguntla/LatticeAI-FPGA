0c7a9e916fd748ff3a8d57dd7d4acb47c19f2e3c
Pranav Gonuguntla <spgonugu@uci.edu>
2025-03-24 12:01:39 -0700

    switching to tensorflow

--- gesture_tracking.py
+++ gesture_tracking.py
@@ -1,13 +1,12 @@
-import torch
-import torch.nn as nn
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-class GestureCNN(nn.Module):  
+class GestureCNN(keras.Model):
     def __init__(self, num_classes=6): # Num_classes depends on the number of different gestures in the dataset (6 is placeholder)
-        super(GestureCNN, self).__init__()  
-        
+        super(GestureCNN, self).__init__()
+
         # Convolutional layers
-        self.conv_one = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.conv_two = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.conv_three = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
-
-
+        self.conv_one = layers.Conv2D(filters=64, kernel_size=3, strides=1, padding="same", activation="relu", input_shape=(64, 64, 1))
+        self.conv_two = layers.Conv2D(filters=128, kernel_size=3, strides=1, padding="same", activation="relu")
+        self.conv_three = layers.Conv2D(filters=256, kernel_size=3, strides=1, padding="same", activation="relu")
