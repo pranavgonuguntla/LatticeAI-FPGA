ec8a86cdf67c250882e0f008f5a6157195bf496f
Pranav Gonuguntla <spgonugu@uci.edu>
2025-03-24 18:38:42 -0700

    added pooling layers

--- gesture_tracking.py
+++ gesture_tracking.py
@@ -6,7 +6,14 @@ class GestureCNN(keras.Model):
     def __init__(self, num_classes=6): # Num_classes depends on the number of different gestures in the dataset (6 is placeholder)
         super(GestureCNN, self).__init__()
 
-        # Convolutional layers
+        # Convolutional layers and pooling layers
         self.conv_one = layers.Conv2D(filters=64, kernel_size=3, strides=1, padding="same", activation="relu", input_shape=(64, 64, 1))
+        self.pool_one = layers.MaxPooling2D(pool_size=(2,2), strides=2)
+
         self.conv_two = layers.Conv2D(filters=128, kernel_size=3, strides=1, padding="same", activation="relu")
+        self.pool_two = layers.MaxPooling2D(pool_size=(2,2), strides=2)
+
         self.conv_three = layers.Conv2D(filters=256, kernel_size=3, strides=1, padding="same", activation="relu")
+        self.pool_three = layers.MaxPooling2D(pool_size=(2,2), strides=2)
+
+        
